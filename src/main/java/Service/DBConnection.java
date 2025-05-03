c4c052d9e7c2072bdb0e87e20cf60168bfa7eeac
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-05-03 11:39:38 +0530

    Update DBConnection.java
    Changed into proper way

--- src/main/java/Service/DBConnection.java
+++ src/main/java/Service/DBConnection.java
@@ -2,22 +2,28 @@ package Service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/autoelite_db";
 	private static final String USER ="root";
 	private static final String PASSWORD = "31415";
 	
-	private static Connection con;
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL,USER,PASSWORD);
+	}
 	
-	public static Connection getConnection() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(URL,USER,PASSWORD);
+	//close connection
+	public static void closeConnection(Connection conn) {
+		if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				System.out.println("Error closing connection: " + e.getMessage());
+			}
 		}
-		catch (Exception e) {
-			System.out.println("Database connection failed!.");
-		}
-		return con;
 	}
 }
