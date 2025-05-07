12aa332d21f53d78d03de52f1632c14a00af29e8
chaninduisuranga <159243993+chaninduisuranga@users.noreply.github.com>
2025-05-07 07:31:09 +0530

    register page create

--- src/main/java/Controller/registerUserServelet.java
+++ src/main/java/Controller/registerUserServelet.java
@@ -1,25 +1,44 @@
 package Controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class registerUserServelet
- */
+import Service.UserDB;
+
+
 @WebServlet("/registerUserServelet")
 public class registerUserServelet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String u_firstname = request.getParameter("u_firstname");
+		String u_lastname = request.getParameter("u_lastname");
+		String u_email = request.getParameter("u_email");
+		String u_password = request.getParameter("u_password");
+		String u_type = request.getParameter("u_type");
+		
+		
+
+		boolean isRegistered = UserDB.registerUser(u_firstname, u_lastname, u_email, u_password, u_type,"profileicon.webp");
+
+		if(isRegistered == true) {
+			response.sendRedirect("Login.jsp");
+			
+		}
+		else {
+			request.setAttribute("RegisterError", "Registration Failed");
+	        RequestDispatcher dis = request.getRequestDispatcher("SignUp.jsp");
+	        dis.forward(request, response);
+		}
+	
 	}
 
 }
