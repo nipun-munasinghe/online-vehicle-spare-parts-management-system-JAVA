2c5fd9e48876a201442a3aa8eaafc02c257373ae
Thimira Thathsarana <studywiththimira@gmail.com>
2025-05-07 13:39:36 +0530

    Login

--- src/main/java/Controller/userLoginServlet.java
+++ src/main/java/Controller/userLoginServlet.java
@@ -1,4 +1,4 @@
-package Controller;
+package Controller; 
 
 import java.io.IOException;
 
@@ -11,27 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import Model.User;
-import Service.LoginDB.UserService;
+import Service.UserDB;
 
 @WebServlet("/userLoginServlet")
 
-public class userLoginServlet extends HttpServlet {
-    private UserService userService = new UserService();
+public class userLoginServlet extends HttpServlet { 
 
-   
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        User user = userService.authenticate(email, password);
+        User user = UserDB.authenticate(email, password);
 
         if (user != null) {
             HttpSession session = request.getSession();
             session.setAttribute("user", user);
-            response.sendRedirect(request.getContextPath() + "/user_profile.jsp");
+            response.sendRedirect(request.getContextPath() + "myProfileServlet.jsp"); 
         } else {
-            request.setAttribute("error", "Invalid email or password");
+            request.setAttribute("errorMessage", "Invalid email or password"); 
             RequestDispatcher dispatcher = request.getRequestDispatcher("Login.jsp");
             dispatcher.forward(request, response);
         }
